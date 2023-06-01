5e13265c0e2a7560c6902729bc40ab3929bbc586
Andis <leinerts59@gmail.com>
2023-06-01 22:16:51 +0300

    First view started, database made, started about

---
+++ database/migrations/2023_06_01_191011_create_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller')->constrained('users');
+            $table->string('name');
+            $table->string('author');
+            $table->string('ISBN');
+            $table->decimal('price', 8, 2);
+            $table->date('release_date');
+            $table->text('condition')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
