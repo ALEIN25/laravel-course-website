6500383e04403ac384f501f6c8a5f37ea549c1ad
Andis <leinerts59@gmail.com>
2023-06-29 18:44:10 +0300

    Fixed redirect when not auth when accsessing auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            $locale = $request->segment(1);
+            return route('login', $locale);
+        }
+    
+        return null;
     }
+    
 }
